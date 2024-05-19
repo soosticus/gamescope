a8da7299995f0d5b35618b07cdb35b9b28f265d2
Matthew Anderson <ruinairas1992@gmail.com>
2024-05-19 08:55:28 -0500

    Fix a typo for --bypass-steam-resolution

--- src/steamcompmgr.cpp
+++ src/steamcompmgr.cpp
@@ -7454,7 +7454,7 @@ steamcompmgr_main(int argc, char **argv)
 					bForceWindowsFullscreen = true;
 				} else if (strcmp(opt_name, "hdr-enabled") == 0 || strcmp(opt_name, "hdr-enable") == 0) {
 					cv_hdr_enabled = true;
-				} else if (strcmp(opt_name, "bypass_steam_resolution") == 0) {
+				} else if (strcmp(opt_name, "bypass-steam-resolution") == 0) {
 					cv_bypass_steam_resolution = true;
 				} else if (strcmp(opt_name, "hdr-debug-force-support") == 0) {
 					g_bForceHDRSupportDebug = true;
